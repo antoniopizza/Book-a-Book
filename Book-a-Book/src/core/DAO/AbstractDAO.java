d7edc2d43d46f278a877e6c99b77058c01b23a15
MBenincasa <m.benincasa13@studenti.unisa.it>
2017-12-18 17:31:27 +0100

    Risolto problemi nelle properties

--- Book-a-Book/src/core/DAO/AbstractDAO.java
+++ Book-a-Book/src/core/DAO/AbstractDAO.java
@@ -3,11 +3,40 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package core.DAO;
 
 /**
  *
  * @author manuel
  */
-public class AbstractDAO {
+public abstract class AbstractDAO<T> {
+    /**
+     * 
+     * @param id
+     * @return 
+     */
+    public abstract T doRetriveById(int id);
+    
+    /**
+     * 
+     * @return 
+     */
+    public abstract T doRetriveAll();
+    
+    /**
+     * 
+     * @param entity
+     * @return 
+     */
+    public abstract int doInsert( T entity);
+    
+    
+    /**
+     * 
+     * @param entity
+     * @return 
+     */
+    public abstract int doUpdate(T entity);
+    
     
 }
