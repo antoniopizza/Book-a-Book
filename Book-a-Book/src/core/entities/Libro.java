2d5a813eff366d9c3a7f090870742ec1e2feef87
MBenincasa <m.benincasa13@studenti.unisa.it>
2017-12-27 17:54:43 +0100

    Revert "Risoluzione di problemi con libroEntity"
    This reverts commit 517a4711e83288a56a1f76f188531ff9bf966091.

--- Book-a-Book/src/core/entities/Libro.java
+++ Book-a-Book/src/core/entities/Libro.java
@@ -6,8 +6,7 @@
 package core.entities;
 
 import java.util.Calendar;
-import java.util.List;
-import java.util.Objects;
+import java.util.Set;
 
 /**
  *
@@ -21,24 +20,22 @@ public class Libro {
     protected Calendar dataPubblicazione;
     protected String descrizione;
     protected boolean disponibilta;
-    protected List<Autore> autori;
-    protected String pathFoto;
+    protected Set<Autore> autori;
 
     public Libro() {
         
     }
 
-    public Libro(String isbn, String titolo, String editore, Calendar dataPubblicazione, String descrizione, boolean disponibilta,String pathFoto) {
+    public Libro(String isbn, String titolo, String editore, Calendar dataPubblicazione, String descrizione, boolean disponibilta) {
         this.isbn = isbn;
         this.titolo = titolo;
         this.editore = editore;
         this.dataPubblicazione = dataPubblicazione;
         this.descrizione = descrizione;
         this.disponibilta = disponibilta;
-        this.pathFoto = pathFoto;
     }
 
-    public Libro(String isbn, String titolo, String editore, Calendar dataPubblicazione, String descrizione, boolean disponibilta, List<Autore> autori,String pathFoto) {
+    public Libro(String isbn, String titolo, String editore, Calendar dataPubblicazione, String descrizione, boolean disponibilta, Set<Autore> autori) {
         this.isbn = isbn;
         this.titolo = titolo;
         this.editore = editore;
@@ -46,7 +43,6 @@ public class Libro {
         this.descrizione = descrizione;
         this.disponibilta = disponibilta;
         this.autori = autori;
-        this.pathFoto = pathFoto;
     }
     
     public String getIsbn() {
@@ -97,67 +93,17 @@ public class Libro {
         this.disponibilta = disponibilta;
     }
 
-    public List<Autore> getAutori() {
+    public Set<Autore> getAutori() {
         return autori;
     }
 
-    public void setAutori(List<Autore> autori) {
+    public void setAutori(Set<Autore> autori) {
         this.autori = autori;
-    }
-
-    public String getPathFoto() {
-        return pathFoto;
-    }
-
-    public void setPathFoto(String pathFoto) {
-        this.pathFoto = pathFoto;
     }
 
     @Override
     public String toString() {
-        return "Libro{" + "isbn=" + isbn + ", titolo=" + titolo + ", editore=" + editore + ", dataPubblicazione=" + dataPubblicazione + ", descrizione=" + descrizione + ", disponibilta=" + disponibilta + ", autori=" + autori + ", pathFoto=" + pathFoto + '}';
+        return "Libro{" + "isbn=" + isbn + ", titolo=" + titolo + ", editore=" + editore + ", dataPubblicazione=" + dataPubblicazione + ", descrizione=" + descrizione + ", disponibilta=" + disponibilta + ", autori=" + autori + '}';
     }
-
-  
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Libro other = (Libro) obj;
-        if (this.disponibilta != other.disponibilta) {
-            return false;
-        }
-        if (!Objects.equals(this.isbn, other.isbn)) {
-            return false;
-        }
-        if (!Objects.equals(this.titolo, other.titolo)) {
-            return false;
-        }
-        if (!Objects.equals(this.editore, other.editore)) {
-            return false;
-        }
-        if (!Objects.equals(this.descrizione, other.descrizione)) {
-            return false;
-        }
-        if (!Objects.equals(this.pathFoto, other.pathFoto)) {
-            return false;
-        }
-        if (!Objects.equals(this.dataPubblicazione, other.dataPubblicazione)) {
-            return false;
-        }
-        if (!Objects.equals(this.autori, other.autori)) {
-            return false;
-        }
-        return true;
-    }
-
-    
             
 }
