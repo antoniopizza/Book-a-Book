1f0c55ef028bdb91f2424a1d1909a702aac7ec48
mdestefano <m.destefano36@studenti.unisa.it>
2017-12-27 20:08:48 +0100

    Completeta e testata AutoreDAO

--- Book-a-Book/src/core/DAO/AutoreDAO.java
+++ Book-a-Book/src/core/DAO/AutoreDAO.java
@@ -21,13 +21,17 @@ import java.util.List;
 public class AutoreDAO extends AbstractDAO<Autore> {
 
     private final String doRetriveByIdQuery = "SELECT * FROM Autore WHERE id = ? ";
+    private final String doRetriveAllQuery = "SELECT * FROM Autore";
     private final String doRetriveByLibroQuery = "SELECT * FROM Autore a JOIN Libro_Autore la "
                                                + "ON a.id = la.id_autore WHERE la.isbn = ? ";
-
+    private final String doRetriveByNomeQuery = "SELECT * FROM Autore WHERE nome = ? ";
+    private final String doInsertQuery = "INSERT INTO Autore(nome) VALUES(?)";
+    private final String doUpdateQuery = "UPDATE Autore SET nome = ? WHERE id = ?";
+    
     /**
-     *
+     * Seleziona un autore in base al suo id
      * @param id[0] si aspetta un codice identificativo di un autore
-     * @return un autore in base al codice id
+     * @return un autore se viene trovato, null atrimenti.
      */
     @Override
     public Autore doRetriveById(Object... id) {
@@ -38,6 +42,7 @@ public class AutoreDAO extends AbstractDAO<Autore> {
             prst.setInt(1, idAutore);
 
             try (ResultSet rs = prst.executeQuery()) {
+                con.commit();
                 Autore a = null;
                 if (rs.next()) {
                     a = new Autore();
@@ -62,11 +67,48 @@ public class AutoreDAO extends AbstractDAO<Autore> {
 
     }
 
+    /**
+     * Seleziona tutti gli autori presenti nel database
+     * @return la lista di tutti gli autori presenti sul db 
+     */
     @Override
     public List<Autore> doRetriveAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Autore> autori = new ArrayList<>();
+        
+         try (Connection con = DriverManagerConnectionPool.getConnection()) {
+            PreparedStatement prst = con.prepareStatement(doRetriveAllQuery);            
+
+            try (ResultSet rs = prst.executeQuery()) { 
+                con.commit();
+                while (rs.next()) {
+                    Autore a = new Autore();
+                    a.setId(rs.getInt("id"));
+                    a.setNome(rs.getString("nome"));
+                    autori.add(a);
+                }
+                rs.close();
+                
+                
+            } catch (SQLException e ){
+                con.rollback();
+            } finally{
+                DriverManagerConnectionPool.releaseConnection(con);                
+                prst.close();
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        
+        return autori;
     }
 
+    /**
+     * Seleziona dal Db tutti gli autori di un dato libro
+     * @param isbn del libro.
+     * @return la lista di autori del libro, una lista vuota altrimenti.
+     */
     public List<Autore> doRetriveByLibro(String isbn) {
         List<Autore> autori = new ArrayList<>();
         
@@ -89,6 +131,43 @@ public class AutoreDAO extends AbstractDAO<Autore> {
                 
             } catch (SQLException e ){
                 con.rollback();
+                e.printStackTrace();
+                
+            } finally{
+                DriverManagerConnectionPool.releaseConnection(con);                
+                prst.close();
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        return autori;
+    }
+    
+    /**
+     * Metodo che seleziona dal Db un autore in base al nome
+     * @param nome
+     * @return un oggetto Autore se viene trovato, null altrimenti.
+     */
+    public Autore doRetriveByNome(String nome){
+         try (Connection con = DriverManagerConnectionPool.getConnection()) {
+            PreparedStatement prst = con.prepareStatement(doRetriveByNomeQuery);
+            prst.setString(1, nome);
+
+            try (ResultSet rs = prst.executeQuery()) {
+                con.commit();
+                Autore a = null;
+                if (rs.next()) {
+                    a = new Autore();
+                    a.setId(rs.getInt("id"));
+                    a.setNome(rs.getString("nome"));
+                }
+                rs.close();
+                return a;
+                
+            } catch (SQLException e ){
+                con.rollback();
                 return null;
                 
             } finally{
@@ -99,18 +178,68 @@ public class AutoreDAO extends AbstractDAO<Autore> {
         } catch (SQLException e) {
             return null;
         }
-        
-        return autori;
     }
 
+    /**
+     * Metodo che inserisce un autore nel database
+     * @param autore da inserire
+     * @return l'id della tupla inserita, -1 altrimenti
+     */
     @Override
     public int doInsert(Autore autore) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+            Connection con = DriverManagerConnectionPool.getConnection();
+            PreparedStatement prst = con.prepareStatement(doInsertQuery,PreparedStatement.RETURN_GENERATED_KEYS);
+            prst.setString(1,autore.getNome());
+            
+            try{
+                prst.execute();
+                con.commit();
+                ResultSet rs = prst.getGeneratedKeys();
+                rs.next();
+                
+                return rs.getInt(1);                                
+                
+            } catch(SQLException e) {
+                con.rollback();
+                e.printStackTrace();
+                return -1;
+            }
+            
+        } catch(SQLException e){
+            e.printStackTrace();
+            return -1;
+        }
     }
 
+    /**
+     * Metodo che aggiorna un autore nel database.
+     * @param autore
+     * @return 
+     */
     @Override
     public int doUpdate(Autore autore) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+            Connection con = DriverManagerConnectionPool.getConnection();
+            PreparedStatement prst = con.prepareStatement(doUpdateQuery);
+            prst.setString(1,autore.getNome());
+            prst.setInt(2,autore.getId());
+            
+            try{
+                prst.execute();
+                con.commit(); 
+                return autore.getId();                
+                
+            } catch(SQLException e) {
+                con.rollback();
+                e.printStackTrace();
+                return -1;
+            }
+            
+        } catch(SQLException e){
+            e.printStackTrace();
+            return -1;
+        }
     }
 
 }
