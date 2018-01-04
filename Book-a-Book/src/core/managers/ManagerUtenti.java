ea6b0b1b6ec3fc7290f2ac515827c8807f4129dc
MBenincasa <m.benincasa13@studenti.unisa.it>
2018-01-04 10:34:54 +0100

    Implementato il metodo per l'eliminazione degli utenti da parte dell'admin

--- Book-a-Book/src/core/managers/ManagerUtenti.java
+++ Book-a-Book/src/core/managers/ManagerUtenti.java
@@ -5,6 +5,12 @@
  */
 package core.managers;
 
+import core.DAO.AccountDAO;
+import core.DAO.BibliotecarioDAO;
+import core.DAO.PersonaDAO;
+import core.entities.Account;
+import core.entities.Bibliotecario;
+import core.entities.Persona;
 import core.entities.Utente;
 import core.utils.Criterio;
 import java.util.Collection;
@@ -20,7 +26,28 @@ public class ManagerUtenti {
     }
     
     public boolean eliminaRegistrati(String email){
-       throw new UnsupportedOperationException("Not implemented yet"); 
+       
+        AccountDAO accountDAO = new AccountDAO();
+        Account account = accountDAO.doRetriveById(email);
+        
+        if(account.getTipo().equals("Persona")){
+            PersonaDAO personaDAO = new PersonaDAO();
+            Persona persona = personaDAO.doRetriveByEmail(email);
+            persona.getAccount().setEmail(null);
+            
+            if(personaDAO.doUpdate(persona) == -1 && accountDAO.doDelete(email) == -1){
+                return false;
+            }
+        } else if(account.getTipo().equals("Bibliotecario")){
+            BibliotecarioDAO bibliotecarioDAO = new BibliotecarioDAO();
+            Bibliotecario bibliotecario = bibliotecarioDAO.doRetriveByEmail(email);
+            
+            if(bibliotecarioDAO.doDelete(bibliotecario) == -1 && accountDAO.doDelete(email) == -1){
+                return false;
+            }
+        }
+        
+        return true;
     }
     
 }
