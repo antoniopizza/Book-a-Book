a38ecdc518ee8ddbad220d38a83fc69a1db86403
mdestefano <m.destefano36@studenti.unisa.it>
2018-01-16 09:27:53 +0100

    Eliminato costruttore inutilizzato in Posizione.java

--- Book-a-Book2/src/main/java/core/entities/Posizione.java
+++ Book-a-Book2/src/main/java/core/entities/Posizione.java
@@ -36,9 +36,6 @@ public class Posizione {
         this.copie = copie;
     }
 
-    public Posizione(String a5, Biblioteca bib) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 
     public List<Copia> getCopie() {
         return copie;
